40e7f70e9ba600e4533d56f74d6ddd873e2ba376
vsponton <2146222@ucc.edu.ar>
2023-05-31 22:07:14 -0300

    Se realizaron cambios

--- funciones.js
+++ funciones.js
@@ -36,9 +36,9 @@ function mostrar_ocultar(valor){
 
 function PerimetroTrian() {
     var Lado1,Lado2,Lado3;
-    Lado1 = Number(document.getElementById("Lado1").value);
-    Lado2 = Number(document.getElementById("Lado2").value);
-    Lado3 = Number(document.getElementById("Lado3").value);
+    Lado1 = document.getElementById("Lado1").value;
+    Lado2 = document.getElementById("Lado2").value;
+    Lado3 = document.getElementById("Lado3").value;
     if (document.getElementById("Triangulo").checked) {
 
         if (Lado1.includes(",")) {
@@ -70,7 +70,7 @@ function PerimetroTrian() {
             alert("Se ingreso un valor incorrecto.Deben ser números");
         }
         if (Lado1>0 && Lado2>0 && Lado3>0 && !isNaN(Lado1) && !isNaN(Lado2)&& !isNaN(Lado3)){
-            document.getElementById("Resultado").value = Lado1+Lado2+Lado3;
+            document.getElementById("Resultado").value = Number (Lado1) + Number(Lado2)+ Number(Lado3);
             localStorage.setItem("Lado1", Lado1);
             localStorage.setItem("Lado2", Lado2);
             localStorage.setItem("Lado3", Lado3);
@@ -86,8 +86,8 @@ function PerimetroTrian() {
 
 function PerimetroRec() {
     var Base,Altura;
-    Base = Number(document.getElementById("Base").value);
-    Altura = Number(document.getElementById("Altura").value);
+    Base = (document.getElementById("Base").value);
+    Altura = (document.getElementById("Altura").value);
     if (document.getElementById("Rectangulo").checked) {
 
         if (Base.includes(",")) {
@@ -108,7 +108,7 @@ function PerimetroRec() {
         if (isNaN(Altura)) {
             alert("Se ingreso un valor incorrecto.Deben ser números ");
         }
-        document.getElementById("Resultado").value = 2*Altura + 2*Base;
+        document.getElementById("Resultado").value = Number(2*Altura) + Number (2*Base);
         localStorage.setItem("Base", Base);
         localStorage.setItem("Altura", Altura);
     }
@@ -123,7 +123,7 @@ function PerimetroRec() {
 
 function PerimetroCuad() {
     var Lado;
-    Lado = Number(document.getElementById("Lado").value);
+    Lado = document.getElementById("Lado").value;
     if (document.getElementById("Cuadrado").checked) {
         if (Lado.includes(",")) {
             Lado = Lado.replace(",", ".");
@@ -134,7 +134,7 @@ function PerimetroCuad() {
         if (isNaN(Lado)) {
             alert("Se ingreso un valor incorrecto.Deben ser números");
         }
-        document.getElementById("Resultado").value = Lado * 4;
+        document.getElementById("Resultado").value = Number(Lado * 4);
         localStorage.setItem("Lado", Lado);
     }
 }
@@ -146,7 +146,7 @@ function PerimetroCuad() {
  */
 function PerimetroCirc() {
     var Radio;
-    Radio = Number(document.getElementById("Radio").value);
+    Radio = document.getElementById("Radio").value;
     if (document.getElementById("Circulo").checked) {
         if (Radio.includes(",")) {
             Radio = Radio.replace(",", ".");
@@ -157,8 +157,115 @@ function PerimetroCirc() {
         if (isNaN(Radio)) {
             alert("Se ingreso un valor incorrecto.Deben ser números ");
         }
-        document.getElementById("Resultado").value = 2 * Math.PI * Radio;
+        document.getElementById("Resultado").value = Number (2 * Math.PI * Radio);
         localStorage.setItem("Radio", Radio);
 
     }
+}
+/**
+ * Función que permite dibujar un triángulo según la longitud de sus tres lados
+ * @method DibujarTriangulo
+ * @param Lado1 - Lado 1 ingresado por el usuario
+ * @param Lado2 - Lado 2 por el usuario
+ * @param Lado3- Lado 3 por el usuario
+ * @return {canvas} dibujo de la figura geométrica
+ */
+
+Posicionx=0;
+function DibujarTriangulo(){
+    var Lado1, Lado2, Lado3;
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = canvas.width;
+    var anchoMax;
+    Lado1 = localStorage.getItem("Lado1");
+    Lado1 = localStorage.getItem("Lado2");
+    Lado3 = localStorage.getItem("Lado3");
+    anchoMax=canvas.width;
+    ctx.beginPath();
+    ctx.moveTo(100+Posicionx, 100);
+    ctx.lineTo(Base/2+Posicionx,Altura);
+    ctx.lineTo(Base+Posicionx,100);
+    ctx.closePath();
+    ctx.strokeStyle = "#bc57d5";
+    ctx.stroke();
+    Posicionx=Posicionx+10;
+    if(Posicionx>canvas.width)
+    {
+        Posicionx=0;
+    }
+}
+/**
+ * Función que permite dibujar un rectángulo según la longitud de su Base y Altura
+ * @method DibujarRectangulo
+ * @param Base - La base ingresada por el usuario
+ * @param Altura - La altura ingresada por el usuario
+ * @return {canvas} dibujo de la figura geométrica
+ */
+
+function DibujarRectangulo(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d")
+    var base, altura;
+    var margen=50;
+    base=localStorage.getItem("Base")*0.1;
+    altura=localStorage.getItem("Altura")*0.1;
+    canvas.width = canvas.width;
+    ctx.fillStyle = "#572f84";
+    ctx.fillRect(Posicionx, 0 + margen, 100*base, 100*altura);
+    Posicionx=Posicionx+10;
+    if(Posicionx>canvas.width)
+    {
+        Posicionx=0;
+    }
+}
+
+/**
+ * Función que permite dibujar un cuadrado según la longitud de su Lado
+ * @method DibujarCuadrado
+ * @param Lado - El lado ingresado por el usuario
+ * @return {canvas} dibujo de la figura geométrica
+ */
+function DibujarCuadrado(Lado){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var margen = 50;
+    var lado;
+    lado=localStorage.getItem("Lado")*0.1;
+    canvas.width = canvas.width;
+    ctx.fillStyle = "#87367e";
+    ctx.fillRect(Posicionx, 0 + margen, 100*lado, 100*lado);
+    Posicionx=Posicionx+10;
+    if(Posicionx>canvas.width)
+    {
+        Posicionx=0;
+    }
+
+}
+
+/**
+ * Función que permite dibujar un círculo según la longitud de su Radio
+ * @method DibujarCirculo
+ * @param Radio - El radio ingresado por el usuario
+ * @return {canvas} dibujo de la figura geométrica
+ */
+function DibujarCirculo(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    //var xmax = canvas.width;
+    var ymax = canvas.height;
+    var radio;
+    radio=localStorage.getItem("Radio")*0.1;
+    canvas.width = canvas.width;
+    ctx.arc(Posicionx, ymax / 2, 20*radio, 0, 2 * Math.PI);
+    ctx.fillStyle = "#87367e";
+    ctx.fill();
+    ctx.stroke();
+    Posicionx=Posicionx+10;
+    if(Posicionx>canvas.width)
+    {
+        Posicionx=0;
+    }
+    ctx.stroke();
+
 }
